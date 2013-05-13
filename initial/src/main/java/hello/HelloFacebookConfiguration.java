9fa4198cb2c9c85dedc2454268e70ff775844eaf
Craig Walls <cwalls@vmware.com>
2013-05-13 15:10:40 -0500

    Add 'initial' project.

---
+++ initial/src/main/java/hello/HelloFacebookConfiguration.java
@@ -0,0 +1,45 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class HelloFacebookConfiguration {
+
+	/*
+	 * THYMELEAF CONFIG
+	 */
+
+	@Bean
+	public TemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("HTML5");
+		return resolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		return resolver;
+	}
+
+} 
